a23d496f7d674ee0c067b9f58c73ee3d9547242c
Ryan <ryancollicutt22@gmail.com>
2021-03-19 02:52:26 -0700

    initialization

---
+++ templates/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Assignment 8</title>
+
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+
+    <div id="main">
+      <div id="specs">
+            <div id="nav">
+              <ul class="nav">
+                <li></li>
+                <li></li>
+                <li></li>
+              </ul>
+            </div>
+      </div>
+      <div id="slanted">
+        <img src="">
+      </div>
+    </div>
+  
+    <!-- <?php
+      // the_comments();
+      // the_commenters();
+     ?> -->
+  </body>
+</html>
